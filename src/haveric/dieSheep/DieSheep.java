5a364e26d878931215bfb634742cd7fb2ce42fb3
Ryan Breuer <breuerryan@gmail.com>
2012-06-30 20:40:53 -0500

    Cleanup

--- src/haveric/dieSheep/DieSheep.java
+++ src/haveric/dieSheep/DieSheep.java
@@ -1,28 +1,26 @@
 package haveric.dieSheep;
 
-import java.util.logging.Logger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DieSheep extends JavaPlugin{
-    final Logger log = Logger.getLogger("Minecraft");
+public class DieSheep extends JavaPlugin {
     private final SheepInteract sheepInteract = new SheepInteract(this);
 
     private Commands commands = new Commands(this);
-    
-	@Override
-	public void onEnable() {
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(sheepInteract, this);
-		
-		Config.init(this);
 
-		getCommand(Commands.getMain()).setExecutor(commands);
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(sheepInteract, this);
 
-	}
+        Config.init(this);
 
-	@Override
-	public void onDisable() {
+        getCommand(Commands.getMain()).setExecutor(commands);
 
-	}
+        }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
--- src/haveric/dieSheep/SheepInteract.java
+++ src/haveric/dieSheep/SheepInteract.java
@@ -12,62 +12,62 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.SheepDyeWoolEvent;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 
-public class SheepInteract implements Listener{
+public class SheepInteract implements Listener {
 
-	DieSheep plugin;
-	public SheepInteract(DieSheep dieSheep) {
-		plugin = dieSheep;
-	}
+    DieSheep plugin;
+    public SheepInteract(DieSheep dieSheep) {
+        plugin = dieSheep;
+    }
 
-	@EventHandler
-	public void sheepShearEvent(PlayerShearEntityEvent event){
-		if (event.getEntity().getType() == EntityType.SHEEP){
-			Sheep sheep = (Sheep) event.getEntity();
-			
-			if (Config.isEnabled(sheep.getWorld().getName())){
-				sheep.setColor(DyeColor.WHITE);
-				Location loc = sheep.getLocation();
-				
-				Wolf wolf = (Wolf)loc.getWorld().spawn(loc, Wolf.class);
-				wolf.setAngry(true);
-				wolf.damage(0, event.getPlayer());
-			}
-		}
-	}
-	@EventHandler
-	public void sheepHurtEvent(EntityDamageEvent event){
-		if (event.getEntityType() == EntityType.SHEEP){
-			Sheep sheep = (Sheep) event.getEntity();
-			
-			if (Config.isEnabled(sheep.getWorld().getName())){
-				sheep.setColor(DyeColor.WHITE);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void sheepDieEvent(EntityDeathEvent event){
-		if (event.getEntityType() == EntityType.SHEEP){
-			Sheep sheep = (Sheep) event.getEntity();
-			
-			if (Config.isEnabled(sheep.getWorld().getName())){
-				sheep.getWorld().createExplosion(sheep.getLocation(), Config.getExplosionDamage());
-				sheep.setFallDistance(100);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void sheepDyeEvent(SheepDyeWoolEvent event){
-		if (event.getEntityType() == EntityType.SHEEP){
-			Sheep sheep = (Sheep) event.getEntity();
-			
-			if (Config.isEnabled(sheep.getWorld().getName())){
-				event.setCancelled(true);
-				sheep.setColor(DyeColor.WHITE);
-				sheep.getWorld().createExplosion(event.getEntity().getLocation(), Config.getExplosionDamage());
-				sheep.setFallDistance(100);
-			}
-		}
-	}
+    @EventHandler
+    public void sheepShearEvent(PlayerShearEntityEvent event) {
+        if (event.getEntity().getType() == EntityType.SHEEP) {
+            Sheep sheep = (Sheep) event.getEntity();
+
+            if (Config.isEnabled(sheep.getWorld().getName())) {
+                sheep.setColor(DyeColor.WHITE);
+                Location loc = sheep.getLocation();
+
+                Wolf wolf = (Wolf) loc.getWorld().spawn(loc, Wolf.class);
+                wolf.setAngry(true);
+                wolf.damage(0, event.getPlayer());
+            }
+        }
+    }
+    @EventHandler
+    public void sheepHurtEvent(EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.SHEEP) {
+            Sheep sheep = (Sheep) event.getEntity();
+
+            if (Config.isEnabled(sheep.getWorld().getName())) {
+                sheep.setColor(DyeColor.WHITE);
+            }
+        }
+    }
+
+    @EventHandler
+    public void sheepDieEvent(EntityDeathEvent event) {
+        if (event.getEntityType() == EntityType.SHEEP) {
+            Sheep sheep = (Sheep) event.getEntity();
+
+            if (Config.isEnabled(sheep.getWorld().getName())) {
+                sheep.getWorld().createExplosion(sheep.getLocation(), Config.getExplosionDamage());
+                sheep.setFallDistance(100);
+            }
+        }
+    }
+
+    @EventHandler
+    public void sheepDyeEvent(SheepDyeWoolEvent event) {
+        if (event.getEntityType() == EntityType.SHEEP) {
+            Sheep sheep = (Sheep) event.getEntity();
+
+            if (Config.isEnabled(sheep.getWorld().getName())) {
+                event.setCancelled(true);
+                sheep.setColor(DyeColor.WHITE);
+                sheep.getWorld().createExplosion(event.getEntity().getLocation(), Config.getExplosionDamage());
+                sheep.setFallDistance(100);
+            }
+        }
+    }
 }
