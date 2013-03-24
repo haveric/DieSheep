e0928b6542a1f11ac8806530920ce2173251b249
Ryan Breuer <breuerryan@gmail.com>
2013-03-23 19:49:10 -0500

    Added Metrics

--- src/haveric/dieSheep/DieSheep.java
+++ src/haveric/dieSheep/DieSheep.java
@@ -1,26 +1,41 @@
 package haveric.dieSheep;
 
+import haveric.dieSheep.mcstats.Metrics;
+
+import java.io.IOException;
+
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DieSheep extends JavaPlugin {
-    private final SheepInteract sheepInteract = new SheepInteract(this);
 
     private Commands commands = new Commands(this);
+
+    private Metrics metrics;
 
     @Override
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(sheepInteract, this);
+        pm.registerEvents(new SheepInteract(this), this);
 
         Config.init(this);
 
         getCommand(Commands.getMain()).setExecutor(commands);
 
-        }
+        setupMetrics();
+    }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void setupMetrics() {
+        try {
+            metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
