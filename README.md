7ebf2659724204b8ad9ae26d7eabedaf3da0f2c8
Ryan Breuer <breuerryan@gmail.com>
2013-09-21 11:00:58 -0500

    Removed Metrics as the service has been discontinued.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ DieSheep
 #### Changelog:
 
 #### Version 1.1
-* Updated Metrics to R7
+* Removed Metrics as the service has been discontinued.
 
 #### Version 1.0
 * Initial version
--- src/haveric/dieSheep/DieSheep.java
+++ src/haveric/dieSheep/DieSheep.java
@@ -1,17 +1,11 @@
 package haveric.dieSheep;
 
-import haveric.dieSheep.mcstats.Metrics;
-
-import java.io.IOException;
-
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DieSheep extends JavaPlugin {
 
     private Commands commands = new Commands(this);
-
-    private Metrics metrics;
 
     @Override
     public void onEnable() {
@@ -21,21 +15,10 @@ public class DieSheep extends JavaPlugin {
         Config.init(this);
 
         getCommand(Commands.getMain()).setExecutor(commands);
-
-        setupMetrics();
     }
 
     @Override
     public void onDisable() {
 
-    }
-
-    private void setupMetrics() {
-        try {
-            metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
